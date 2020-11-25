b704e300ff95b0c3fda47075d7cb794df20f6bf8
Fabian Siegel <fabian@livelyapps.com>
2020-11-25 12:58:07 +0100

    Added Type Hints to TlsAnaylzer

--- analyzers/tls_analyzer.py
+++ analyzers/tls_analyzer.py
@@ -1,4 +1,7 @@
-from models.requirements import RequirementsResult
+import typing
+
+from models.requirements import RequirementsResult, Requirements
+from models.tls_result import TlsResult
 from reports.constants import (CERT_NOT_VALID, HSTS_MISSING, NO_ISSUES,
                                REQ_TITLES, TLS_PROTOCOLS)
 
@@ -6,13 +9,13 @@ PROTO_DENYLIST = ['TLS_1_0', 'TLS_1_1', 'SSL_3_0', 'SSL_2_0']
 
 
 class TlsAnalyzer(object):
-    def __init__(self, tls_scan, requirements):
-        self.scan = tls_scan
-        self.reqs = requirements
+    def __init__(self, tls_scan: TlsResult, requirements: Requirements):
+        self.scan: TlsResult = tls_scan
+        self.reqs: Requirements = requirements
 
-    def _check_tls_versions(self):
+    def _check_tls_versions(self) -> typing.Tuple[bool, typing.List[str]]:
         passed = True
-        proof = []
+        proof: typing.List[str] = []
 
         uses_bad_protos = any(item in self.scan.protocols for item in PROTO_DENYLIST)
         if uses_bad_protos:
@@ -21,26 +24,26 @@ class TlsAnalyzer(object):
 
         return passed, proof
 
-    def _check_hsts(self):
+    def _check_hsts(self) -> typing.Tuple[bool, typing.List[str]]:
         passed = self.scan.hsts_present
-        proof = []
+        proof: typing.List[str] = []
 
         if not passed:
             proof += ['We did not detect an HSTS header when scanning your app.']
 
         return passed, proof
 
-    def _check_cert_valid(self):
+    def _check_cert_valid(self) -> typing.Tuple[bool, typing.List[str]]:
         passed = self.scan.trusted
-        proof = []
+        proof: typing.List[str] = []
 
         if not passed:
             proof += ['Your app presented an HTTPS certificate that was not valid.']
 
         return passed, proof
 
-    def _determine_description(self, passed, tls, hsts):
-        res = []
+    def _determine_description(self, passed, tls, hsts) -> typing.List[str]:
+        res: typing.List[str] = []
         if passed:
             return [NO_ISSUES]
         if not tls:
@@ -50,7 +53,7 @@ class TlsAnalyzer(object):
 
         return res
 
-    def analyze(self):
+    def analyze(self) -> Requirements:
         tls_passed, tls_proof = self._check_tls_versions()
         hsts_passed, hsts_proof = self._check_hsts()
         passed = tls_passed and hsts_passed
