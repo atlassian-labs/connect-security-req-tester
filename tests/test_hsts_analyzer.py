09e1f12bd39d216562c52bdc05c7d7ddc90d1406
Sean Marpo <smarpo@atlassian.com>
2021-05-18 09:52:17 -0700

    Follow redirects when checking for HSTS

--- tests/test_hsts_analyzer.py
+++ tests/test_hsts_analyzer.py
@@ -31,6 +31,6 @@ def test_invalid_scan_header():
     assert res.req1_2.passed is False
     assert res.req1_2.description == [HSTS_MISSING]
     assert res.req1_2.proof == [
-        'We did not detect an HSTS header when scanning your app.'
+        'We did not detect an HSTS header when scanning your app\'s baseUrl.'
     ]
     assert res.req1_2.title == REQ_TITLES['1.2']
