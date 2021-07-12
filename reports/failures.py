c68da478a709008e93ce3196b4d2460120733f54
Anshuman Bhartiya <abhartiya@atlassian.com>
2021-07-12 13:03:00 -0700

    Generating JSON report for app failures

--- reports/failures.py
+++ reports/failures.py
@@ -1,9 +1,11 @@
 import logging
+import json
+import reports.constants
+
 from pathlib import Path
 from typing import List
 from models.requirements import Results
-
-import reports.constants
+from models.vulnerability import AppFailureReport
 
 
 class FailureGenerator(object):
@@ -22,22 +24,35 @@ class FailureGenerator(object):
     def _get_report_path(self, fname: str) -> str:
         return str(Path(self.out_dir + '/' + fname).resolve())
 
+    def _create_json_report(self) -> AppFailureReport:
+        app_failures_report = AppFailureReport(
+            app_key=self.results.key,
+            app_name=self.results.name,
+            descriptor_url=self.descriptor_url
+        )
+        return app_failures_report
+
+    def _write_output(self, contents: str, json_name: str):
+        with open(self._get_report_path(json_name), 'w') as file:
+            file.write(contents)
+        logging.info(f"Wrote report to: {self._get_report_path(json_name)}")
+
     def save_failures(self):
+        json_report = self._create_json_report()
+
         timeout_fname = f"timeouts_{self.timeout}s"
         service_unavailable_fname = f"service_unavailable_{self.timeout}s"
         infinite_redirects_fname = f"infinite_redirects_{self.timeout}s"
-        fname = ""
+        json_name = ""
 
         if "timeouts" in self.results.errors:
             Path(self.out_dir + '/' + timeout_fname).mkdir(exist_ok=True, parents=True)
-            fname = f"{timeout_fname}/{self.results.key}.csv"
+            json_name = f"{timeout_fname}/{self.results.key}.json"
         if "service_unavailable" in self.results.errors:
             Path(self.out_dir + '/' + service_unavailable_fname).mkdir(exist_ok=True, parents=True)
-            fname = f"{service_unavailable_fname}/{self.results.key}.csv"
+            json_name = f"{service_unavailable_fname}/{self.results.key}.json"
         if "infinite_redirects" in self.results.errors:
             Path(self.out_dir + '/' + infinite_redirects_fname).mkdir(exist_ok=True, parents=True)
-            fname = f"{infinite_redirects_fname}/{self.results.key}.csv"
-        with open(self._get_report_path(fname), 'w') as file:
-            file.write(self.descriptor_url)
-            file.close()
-        logging.info(f"Wrote failures to: {self._get_report_path(fname)}")
+            json_name = f"{infinite_redirects_fname}/{self.results.key}.json"
+
+        self._write_output(json.dumps(json_report.to_json()), json_name)
